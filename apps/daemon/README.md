af538014348aaf55f2a839a9cc272d0e73cd8021
ray <me@rayhanadev.com>
2025-02-15 06:06:05 -0500

    feat(daemon): implement local-use repl

--- apps/daemon/README.md
+++ apps/daemon/README.md
@@ -2,3 +2,42 @@
 
 The Blue daemon is a service that runs in the background and manages container
 lifecycle and networking for the entire Blue system.
+
+You can enter into a REPL to interact with the agent in the container.
+
+```sh
+$ bun run src/index.ts repl
+
+Welcome to the REPL!
+Type 'help' for a list of commands.
+✔ > ping
+{
+  "$typeName": "ping.PongResponse",
+  "message": "pong"
+}
+✔ > exec {"command":"ls","args":["-la","/"]}
+total 60
+drwxr-xr-x   1 root root 4096 Feb 15 09:22 .
+drwxr-xr-x   1 root root 4096 Feb 15 09:22 ..
+-rwxr-xr-x   1 root root    0 Feb 15 09:22 .dockerenv
+drwxr-xr-x   1 root root 4096 Feb 15 09:14 app
+lrwxrwxrwx   1 root root    7 Jan 26 02:16 bin -> usr/bin
+drwxr-xr-x   2 root root 4096 Apr 18  2022 boot
+drwxr-xr-x   5 root root  340 Feb 15 09:22 dev
+drwxr-xr-x   1 root root 4096 Feb 15 09:22 etc
+drwxr-xr-x   2 root root 4096 Apr 18  2022 home
+lrwxrwxrwx   1 root root    7 Jan 26 02:16 lib -> usr/lib
+drwxr-xr-x   2 root root 4096 Jan 26 02:16 media
+drwxr-xr-x   2 root root 4096 Jan 26 02:16 mnt
+drwxr-xr-x   2 root root 4096 Jan 26 02:16 opt
+dr-xr-xr-x 235 root root    0 Feb 15 09:22 proc
+drwx------   2 root root 4096 Jan 26 02:23 root
+drwxr-xr-x   5 root root 4096 Jan 26 02:23 run
+lrwxrwxrwx   1 root root    8 Jan 26 02:16 sbin -> usr/sbin
+drwxr-xr-x   2 root root 4096 Jan 26 02:16 srv
+dr-xr-xr-x  11 root root    0 Feb 15 09:22 sys
+drwxrwxrwt   2 root root 4096 Jan 26 02:23 tmp
+drwxr-xr-x  11 root root 4096 Jan 26 02:16 usr
+drwxr-xr-x  11 root root 4096 Jan 26 02:23 var
+? >
+```
--- apps/daemon/src/index.ts
+++ apps/daemon/src/index.ts
@@ -1,5 +1,121 @@
+import sade from "sade";
+import { input as prompt } from "@inquirer/prompts";
+
 import { client } from "./utils/connect";
 
-const res = await client.ping({});
+const cli = sade("blue");
 
-console.log(res);
+cli.version("1.0.0");
+
+cli
+  .command("start")
+  .describe("Start the daemon")
+  .action(() => {
+    console.log("Starting daemon...");
+  });
+
+cli
+  .command("stop")
+  .describe("Stop the daemon")
+  .action(() => {
+    console.log("Stopping daemon...");
+  });
+
+cli
+  .command("restart")
+  .describe("Restart the daemon")
+  .action(() => {
+    console.log("Restarting daemon...");
+  });
+
+cli
+  .command("status")
+  .describe("Check the status of the daemon")
+  .action(() => {
+    console.log("Checking daemon status...");
+  });
+
+cli
+  .command("repl")
+  .describe("Enter a REPL to execute commands")
+  .action(async () => {
+    console.log("Welcome to the REPL!");
+    console.log("Type 'help' for a list of commands.");
+
+    let exit = false;
+
+    while (!exit) {
+      const input = await prompt({
+        message: ">",
+        validate: (input) => input.length > 0,
+      });
+
+      const [command, ...args] = input.split(" ");
+
+      switch(command) {
+        case "ping": {
+          const res = await client.ping({});
+          console.log(JSON.stringify(res, null, 2));
+          break;
+        }
+        case "exec": {
+          try {
+            const data = JSON.parse(args.join(" "));
+
+            const res = client.executeCommand({
+              command: data.command,
+              args: data.args,
+            });
+
+            for await (const chunk of res) {
+              if (chunk.isError) {
+                process.stderr.write(chunk.output);
+              } else {
+                process.stdout.write(chunk.output);
+              }
+            }
+          } catch (error) {
+            console.error(error);
+          }
+
+          break;
+        }
+        case "help": {
+          console.log("Available commands:");
+          console.log("  ping");
+          console.log("  exec");
+          console.log("  help");
+          console.log("  clear");
+          console.log("  exit");
+          break;
+        }
+        case "clear": {
+          console.log("\033[2J")
+          break;
+        }
+        case "exit": {
+          exit = true;
+          break;
+        }
+        default: {
+          console.error("Invalid command");
+          break;
+        }
+      }
+    }
+  });
+
+cli.parse(process.argv);
+
+// const res = client.executeCommand({
+//   command: "ls",
+//   args: ["-la", "/"],
+// });
+
+// for await (const chunk of res) {
+//   if (chunk.isError) {
+//     process.stderr.write(chunk.output);
+//   } else {
+//     process.stdout.write(chunk.output);
+//   }
+// }
